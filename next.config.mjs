c7550908935d8dd5b7350a87b32631567a1284d3
ikholil <ikholil.bd@gmail.com>
2024-07-05 17:04:02 +0600

    added env in next config

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,13 @@ const nextConfig = {
       },
     ],
   },
+  env: {
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    GOOGLE_APP_CLIENT_ID: process.env.GOOGLE_APP_CLIENT_ID,
+    GOOGLE_APP_CLIENT_SECRET: process.env.GOOGLE_APP_CLIENT_SECRET,
+    DATABASE_URL: process.env.DATABASE_URL,
+    JWT_SECRET: process.env.JWT_SECRET,
+  },
 };
 
 export default nextConfig;
